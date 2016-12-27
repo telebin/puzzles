c94373f6a342f4a6bc4e4b63d65a7e618f77af3c
Adrian Sitko <adrian.sitko@nokia.com>
2016-12-27 09:50:25 +0100

    Added days 20 and 21 stuff (with solutions)

---
+++ adv20.rb
@@ -0,0 +1,138 @@
+require './test_framework'
+
+# returns:
+#  1 if other is HIGHER than self
+# -1 if other is LOWER than self
+#  2 if other contains self
+# -2 if other is contained by self
+class Range
+  def fits? other
+    # p "comparing #{self} and #{other}"
+    return 2 if self.begin >= other.begin and self.last <= other.last
+    return -2 if self.begin <= other.begin and self.last >= other.last
+    return 1 if self.include? (other.begin-1) and not self.include? other.last
+    return -1 if self.include? (other.last+1) and not self.include? other.begin
+    0
+  end
+end
+
+class Adv20Base < TestFramework
+  def get_ranges t
+    ranges = t.map {|range| r = range.split('-'); (r[0].to_i..r[1].to_i) }.sort_by {|r| r.begin}
+    @combined = []
+    ranges.each do |range|
+      index = @combined.find_index {|c| range.fits? c}
+      # p "found index: #{index}"
+      if index
+        case range.fits? @combined[index]
+          when 0
+            # p "0: comb << #{range}"
+            @combined << range
+          when -2
+            # p "-2: #{range}"
+            @combined[index] = range
+          when 1
+            # p "1: (#{range.begin}..#{@combined[index].last})"
+            @combined[index] = (range.begin..(@combined[index].last))
+          when -1
+            # p "-1: (#{@combined[index].begin}..#{range.last})"
+            @combined[index] = (@combined[index].begin..(range.last))
+          else
+            # p "else: #{range.fits? @combined[index]}, next"
+            next
+        end
+      else
+        # p "after case: #{range}"
+        @combined << range
+      end
+      recombine
+    end
+    @combined
+  end
+  
+  def recombine
+    @combined = @combined.sort_by {|c| c.begin}
+    if @combined.each_cons(2) { |l,r| break true if (l.fits? r) != 0 }
+      # puts 'recombining ' + @combined.to_s
+      new_comb = []
+      @combined.each do |combined|
+        index = new_comb.find_index {|c| combined.fits? c}
+        if index
+          case index
+            when 0
+              new_comb << combined
+            when -2
+              new_comb[index] = combined
+            when -1
+              new_comb[index] << (combined.begin..(new_comb[index].last))
+            when 1
+              new_comb[index] << (new_comb[index].begin..(combined.last))
+          end
+        else
+          new_comb << combined
+        end
+      end
+      @combined = new_comb
+      # puts 'recombined ' + @combined.to_s
+    else
+      puts 'no need to recombine ' + @combined.to_s
+    end
+  end
+end
+
+class Adv20AltBase < TestFramework
+  def get_ranges t
+    raw_values = t.map { |range|
+      l,r = range.split('-'); [{v: l.to_i, from: true},{v: r.to_i, from: false}]
+    }.flatten.sort_by { |r| r[:v] }
+    p raw_values
+    ranges = []
+    open_range = nil
+    closed_range = nil
+    raw_values.each do |r|
+      if r[:from]
+        if open_range
+          if closed_range
+            unless closed_range + 1 >= r[:v]
+              p "1: (#{open_range}..#{closed_range})"
+              ranges << (open_range..closed_range)
+              open_range = r[:v]
+            end
+            closed_range = nil
+          end
+        else
+          open_range = r[:v]
+        end
+      else
+        closed_range = r[:v]
+      end
+    end
+    ranges
+  end
+end
+
+class Adv20_a < Adv20AltBase
+  def logic(t)
+    get_ranges(t).first.last + 1
+  end
+end
+
+class Adv20_b < Adv20AltBase
+  def logic(t)
+    ranges = get_ranges t
+    allowed = 0
+    ranges.each_cons(2) do |l,r|
+      puts "range (#{l.last}..#{r.begin}), allowed: #{r.begin - (l.last + 1)}"
+      allowed += r.begin - (l.lats + 1)
+    end
+    allowed
+  end
+
+end
+
+# testa = Adv20_a.new({['5-8','0-2','4-7'] => 3}, 20)
+# testa.test
+# puts testa.run
+
+testb = Adv20_b.new({}, 20)
+puts testb.run
---
+++ adv21.rb
@@ -0,0 +1,85 @@
+require './test_framework'
+
+class Adv21Base < TestFramework
+  def scramble(t)
+    t.each do |instr|
+      # puts "doing: #{instr}"
+      case instr
+        when /swap position (\d+) with position (\d+)/
+          swap_pos $1.to_i, $2.to_i
+        when /swap letter (\w) with letter (\w)/
+          swap_letter $1, $2
+        when /rotate (left|right) (\d+) steps?/
+          rot ($1 == 'left' ? $2.to_i : -$2.to_i)
+        when /rotate based on position of letter (\w)/
+          rot_on $1
+        when /reverse positions (\d+) through (\d+)/
+          rev_pos $1.to_i, $2.to_i
+        when /move position (\d+) to position (\d+)/
+          move $1.to_i, $2.to_i
+        else
+          raise "error: Unknown code"
+      end
+      # puts "effect: #{@to_modif.join}"
+    end
+    # puts "scrambled #{@to_modif.join}"
+    @to_modif.join
+  end
+
+  def swap_pos pos, with
+    tmp = @to_modif[pos]
+    @to_modif[pos] = @to_modif[with]
+    @to_modif[with] = tmp
+  end
+  def swap_letter letter, with
+    letter_pos = @to_modif.find_index letter
+    @to_modif[@to_modif.find_index(with)] = letter
+    @to_modif[letter_pos] = with
+  end
+  def rot by
+    @to_modif.rotate! by
+  end
+  def rot_on letter
+    char_pos = @to_modif.find_index(letter)
+    rot -(char_pos + (char_pos >= 4 ? 2 : 1))
+  end
+  def rev_pos from, to
+    @to_modif[from..to] = @to_modif[from..to].reverse
+  end
+  def move pos, to
+    @to_modif.insert to, @to_modif.delete_at(pos)
+  end
+end
+
+class Adv21_a < Adv21Base
+  def logic(t)
+    @to_modif = $string.chars
+    scramble t
+  end
+end
+
+class Adv21_b < Adv21Base
+  def logic(t)
+    $string.chars.permutation do |perm|
+      @to_modif = perm.clone
+      # printf "checking #{perm.join} - "
+      break perm.join if scramble(t) == $string
+    end
+  end
+end
+
+scrambling_technique = ['swap position 4 with position 0', 'swap letter d with letter b', 'reverse positions 0 through 4',
+  'rotate left 1 step', 'move position 1 to position 4', 'move position 3 to position 0', 'rotate based on position of letter b',
+  'rotate based on position of letter d']
+
+testa = Adv21_a.new({scrambling_technique => 'decab'}, 21)
+$string = 'abcde'
+testa.test
+$string = 'abcdefgh'
+puts testa.run
+
+testb = Adv21_b.new({}, 21)
+$string = 'dgfaehcb'
+puts testb.run
+$string = 'fbgdceah'
+puts testb.run
