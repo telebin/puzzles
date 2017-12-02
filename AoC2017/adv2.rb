2b70d6172cd4817b8c1c5431437453ecb9bb8290
telebin <telebin@sony>
2017-12-02 13:14:42 +0100

    Solution and story for part 2 day 2

--- AoC2017/adv2.rb
+++ AoC2017/adv2.rb
@@ -12,7 +12,14 @@ end
 
 class Adv2_b < TestFramework
   def logic(t)
-    
+    lines = t.split "\n"
+    lines.reduce(0) do |acc, line|
+      mapped = line.split.map(&:to_i)
+      acc += mapped.each do |num|
+        found = mapped.find { |div| div != num && num % div == 0 }
+        break num / found if found
+      end
+    end
   end
 end
 
@@ -22,9 +29,12 @@ adva = Adv2_a.new({'5 1 9 5
 7 5 3
 2 4 6 8
 ' => 18})
-adva.test
-puts adva.logic INPUT
+# adva.test
+# puts adva.logic INPUT
 
-advb = Adv2_b.new({})
+advb = Adv2_b.new({'5 9 2 8
+9 4 7 3
+3 8 6 5
+' => 9})
 # advb.test
-# puts advb.logic INPUT
+puts advb.logic INPUT
