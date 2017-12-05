b4f8c092321c87345bb734c604d2f74dd8611676
Adrian Sitko <adrian.sitko@nokia.com>
2017-12-05 07:25:48 +0100

    Solution for day 4 part 2

--- AoC2017/adv4.rb
+++ AoC2017/adv4.rb
@@ -8,7 +8,10 @@ end
 
 class Adv4_b < TestFramework
   def logic(t)
-
+    t.split("\n").map do |line|
+      sorted_tokens = line.split.map {|tok| tok.split('').sort.join}
+      sorted_tokens == sorted_tokens.uniq
+    end
   end
 end
 
@@ -17,9 +20,15 @@ adva = Adv4_a.new({
                       'aa bb cc dd aa' => [false],
                       'aa bb cc dd aaa' => [true]
                   }, 4)
-adva.test
-p adva.run.partition {|tf| tf}.map {|tf| tf.size}
+# adva.test
+# p adva.run.partition {|tf| tf}.map {|tf| tf.size}
 
-advb = Adv4_b.new({}, 4)
-# advb.test
-# puts advb.run
+advb = Adv4_b.new({
+    'abcde fghij' => [true],
+    'abcde xyz ecdab' => [false],
+    'a ab abc abd abf abj' => [true],
+    'iiii oiii ooii oooi oooo' => [true],
+    'oiii ioii iioi iiio' => [false]
+                  }, 4)
+advb.test
+p advb.run.partition {|tf| tf}.map {|tf| tf.size}
