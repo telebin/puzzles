68c8a1b0e841532ef0b23e3f9fe630291f52aa21
Adrian Sitko <adrian.sitko@nokia.com>
2017-12-05 09:01:31 +0100

    Story day 5 part 2

--- AoC2017/adv5.rb
+++ AoC2017/adv5.rb
@@ -21,9 +21,9 @@ class Adv5_b < TestFramework
 end
 
 adva = Adv5_a.new({"0\n3\n0\n1\n-3\n" => 5}, 5)
-adva.test
-puts adva.run
+# adva.test
+# puts adva.run
 
 advb = Adv5_b.new({}, 5)
-# advb.test
-# puts advb.run
+advb.test
+puts advb.run
