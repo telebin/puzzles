1de2ad8af1ab3dac0c87a10b1f973d96564012cc
Adrian Sitko <adrian.sitko@nokia.com>
2017-12-05 09:01:00 +0100

    Solution day 5 part 1

--- AoC2017/adv5.rb
+++ AoC2017/adv5.rb
@@ -2,7 +2,15 @@ require '../test_framework'
 
 class Adv5_a < TestFramework
   def logic(t)
-
+    counter = 0
+    pos = 0
+    prog = t.split.map(&:to_i)
+    while (0...prog.size).include? pos
+      counter += 1
+      prog[pos] += 1
+      pos += prog[pos] - 1
+    end
+    counter
   end
 end
 
