5b1cfb5938491d9b10485f2706fa083a719510e8
Adrian Sitko <adrian.sitko@nokia.com>
2017-12-05 12:03:46 +0100

    Solution day 5 part 2

--- AoC2017/adv5.rb
+++ AoC2017/adv5.rb
@@ -16,7 +16,16 @@ end
 
 class Adv5_b < TestFramework
   def logic(t)
-
+    counter = 0
+    pos = 0
+    prog = t.split.map(&:to_i)
+    while (0...prog.size).include? pos
+      counter += 1
+      steps_to_take = prog[pos]
+      prog[pos] += (steps_to_take >= 3 ? -1 : 1)
+      pos += steps_to_take
+    end
+    counter
   end
 end
 
@@ -24,6 +33,6 @@ adva = Adv5_a.new({"0\n3\n0\n1\n-3\n" => 5}, 5)
 # adva.test
 # puts adva.run
 
-advb = Adv5_b.new({}, 5)
+advb = Adv5_b.new({"0\n3\n0\n1\n-3\n" => 10}, 5)
 advb.test
 puts advb.run
