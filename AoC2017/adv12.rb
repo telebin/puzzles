03c006530cce1641a4d7d79eb4531e349463762c
Adrian Sitko <adrian.sitko@nokia.com>
2017-12-12 07:04:54 +0100

    Day 12

--- AoC2017/adv12.rb
+++ AoC2017/adv12.rb
@@ -4,22 +4,62 @@ def log(msg)
   STDERR.puts msg.to_s
 end
 
-class Adv12_a < TestFramework
-  def logic(t)
+class Program
 
+  attr_accessor :color
+  attr_reader :id, :neighbours
+
+  def initialize(line)
+    /(?<id>\d+) <-> (?<neigh>.+)/ =~ line
+    @id = id.to_i
+    @neighbours = neigh.split(',').map(&:to_i)
+    # log "Produced program #{@id} #{@neighbours}"
   end
 end
 
-class Adv12_b < TestFramework
-  def logic(t)
-
+class Adv12Base < TestFramework
+  def colorize(prog, color = 0)
+    # log "coloring #{prog}"
+    prog.color = color
+    prog.neighbours.map { |neigh_id| @programs[neigh_id] }.select { |p| !p.color }.each { |neigh| colorize neigh, color }
   end
 end
 
-adva = Adv12_a.new({}, 12)
+class Adv12_a < Adv12Base
+  def logic(t)
+    @programs = t.split("\n").map { |line| Program.new line }.reduce([]) { |arr, prog| arr[prog.id] = prog; arr }
+    colorize @programs[0]
+    @programs.select { |prog| prog.color }.count
+  end
+end
+
+class Adv12_b < Adv12Base
+  def logic(t)
+    @programs = t.split("\n").map { |line| Program.new line }.reduce([]) { |arr, prog| arr[prog.id] = prog; arr }
+    @programs.lazy.select { |prog| !prog.color }.each { |prog|
+      colorize prog, (Random::rand * 0xffffffff).to_i
+    }
+    @programs.group_by { |prog| prog.color }.count
+  end
+end
+
+adva = Adv12_a.new({ '0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5' => 6
+                   }, 12)
 # adva.test
 # puts adva.run
 
-advb = Adv12_b.new({}, 12)
-# advb.test
-# puts advb.run
+advb = Adv12_b.new({ '0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5' => 2 }, 12)
+advb.test
+puts advb.run
