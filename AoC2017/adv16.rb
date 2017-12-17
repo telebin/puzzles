e91d0c78f5822cb9bec7509b3b43c83ac12809f6
telebin <telebin@sony>
2017-12-17 06:55:48 +0100

    Part one of day 16 :(

---
+++ AoC2017/adv16.rb
@@ -0,0 +1,80 @@
+require '../test_framework'
+
+def log(msg)
+  STDERR.puts msg.to_s
+end
+
+class Array
+  def swap!(l, r)
+    unless l.kind_of?(Numeric) || r.kind_of?(Numeric)
+      l = find_index(l)
+      r = find_index(r)
+    end
+    tmp = self[l]
+    self[l] = self[r]
+    self[r] = tmp
+  end
+end
+
+class Adv16_a < TestFramework
+  def logic(t)
+    instructions = t.split(',')
+    dancers = ('a'..'p').to_a
+    instructions.each do |instr|
+      case instr[0]
+        when 's'
+          spin = instr[1, instr.length].to_i
+          dancers = (dancers * 2)[dancers.count - spin, dancers.count]
+        when 'x'
+          /(\d+)\/(\d+)/ =~ instr
+          dancers.swap! $1.to_i, $2.to_i
+        when 'p'
+          /(\w)\/(\w)/ =~ instr
+          dancers.swap! $1, $2
+        else
+          raise 'invalid op'
+      end
+    end
+    dancers.join
+  end
+end
+
+class Adv16_b < TestFramework
+  def logic(t)
+    instructions = t.split(',')
+    dancers = ('a'..'p').to_a
+    results = []
+    counter = 0
+    1e3.to_i.times do
+      instructions.each do |instr|
+        case instr[0]
+          when 's'
+            spin = instr[1, instr.length].to_i
+            dancers = (dancers * 2)[dancers.count - spin, dancers.count]
+          when 'x'
+            /(\d+)\/(\d+)/ =~ instr
+            dancers.swap! $1.to_i, $2.to_i
+          when 'p'
+            /(\w)\/(\w)/ =~ instr
+            dancers.swap! $1, $2
+          else
+            raise 'invalid op'
+        end
+        puts "Repeated #{dancers.join} on #{results.find_index dancers} index now #{counter} " if results.include? dancers
+        results << dancers.dup
+        counter += 1
+      end
+    end
+    dancers.join
+  end
+end
+
+adva = Adv16_a.new({}, 16)
+# adva.test
+# puts adva.run
+
+advb = Adv16_b.new({}, 16)
+# advb.test
+start = Time.now
+puts advb.run
+puts "#{(Time.now - start)} s"
