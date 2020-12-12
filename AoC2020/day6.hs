07f8965fdc95ea9fb846918ce19fd3203304d84a
Adrian Sitko <asitk@softserveinc.com>
2020-12-12 09:28:31 +0100

    Day 6 of 2020 done

--- AoC2020/day6.hs
+++ AoC2020/day6.hs
@@ -1,15 +1,20 @@
 module Main where
 
-import Data.List
+import Data.List hiding (group)
 
-join :: [String] -> [String]
-join = snd . foldl reductor ("", [])
+groupJoined :: [String] -> [String]
+groupJoined = snd . foldl reductor ("", [])
   where
     reductor :: (String, [String]) -> String -> (String, [String])
     reductor (cur, result) "" = ("", cur : result)
     reductor (cur, result) el = (cur ++ el, result)
 
+group :: [String] -> [[String]]
+group = map (dropWhile (== "")) . groupBy (\_ r -> r /= "")
+
 main = do
     contents <- readFile "day6input.txt"
-    print $ sum $ map (length . nub) $ join $ lines contents
+    print $ sum $ map (length . nub) $ groupJoined $ lines contents ++ [""]
+    print $ sum $ map (length . foldl1 intersect) $ group $ lines contents
+    --print $ group $ lines contents
 
