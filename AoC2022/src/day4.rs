1d447686b562d870ac00934a28336976478166bb
telebin <telebin@sony>
2022-12-04 10:12:59 +0100

    Day4 2022 added

---
+++ AoC2022/src/day4.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::ops::Range;
+
+fn parse_line(line: &str) -> ((i32, i32), (i32, i32)) {
+    let ranges = line.split(",")
+        .map(|r| {
+            let nums = r.split("-").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+            (nums[0], nums[1])
+        })
+        .collect::<Vec<(i32, i32)>>();
+    (ranges[0], ranges[1])
+}
+
+pub fn day4() {
+    let data = fs::read_to_string("inputs/day4.input").expect("the file should be there");
+    let data = data.trim();
+
+    println!("{}", data.split("\n")
+        .map(|l| parse_line(l))
+        .filter(|(l, r)|
+            l.0 <= r.0 && l.1 >= r.1 || r.0 <= l.0 && r.1 >= l.1)
+        .count());
+
+    println!("{}", data.split("\n")
+        .map(|l| parse_line(l))
+        .filter(|((ls, le), (rs, re))| // left/right, start/end
+            ls <= re && ls >= rs
+                || le >= rs && le <= re
+                || rs <= le && rs >= ls
+                || re >= ls && re <= le)
+        .count());
+}
--- AoC2022/src/main.rs
+++ AoC2022/src/main.rs
@@ -1,13 +1,21 @@
+use std::collections::HashMap;
+use std::env;
 use day1::day1;
 use day2::day2;
 use day3::day3;
+use day4::day4;
+use day5::day5;
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
+mod day5;
 
 fn main() {
     // day1();
     // day2();
-    day3();
+    // day3();
+    day4();
+    // day5();
 }
