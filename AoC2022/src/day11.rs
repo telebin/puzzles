d9ce3796b62e65c790b10ad5efdfbaf4eddddb81
telebin <telebin@sony>
2022-12-12 10:53:57 +0100

    Refactored day 11 2022

--- AoC2022/src/day11.rs
+++ AoC2022/src/day11.rs
@@ -61,48 +61,22 @@ pub fn day11(path: &str) {
     let data = fs::read_to_string(path).expect("the file should be there");
     let data = data.trim();
 
-    part_one(data);
-    part_two(data);
+    do_work(data, 20, part_one);
+    do_work(data, 10_000, part_two);
 }
 
-fn part_one(data: &str) {
-    let mut monkeys = data.split("\n\n")
-        .map(Monkey::new)
-        .collect::<Vec<Monkey>>();
-    for _ in 0..20 {
-        for midx in 0..monkeys.len() {
-            let monkey: &mut Monkey = &mut monkeys[midx];
-            let mut throws = Vec::new();
-            while let Some(worry_lv) = monkey.items.pop_front() {
-                monkey.inspections += 1;
-                let worry_lv = monkey.operate(worry_lv);
-                let worry_lv = (worry_lv as f64 / 3.0) as i64;
-                let recipient =
-                    if worry_lv % monkey.test_val == 0 { monkey.on_true } else { monkey.on_false };
-                throws.push((worry_lv, recipient));
-            }
-            for (what, who) in throws {
-                let x = monkeys.get_mut(who).unwrap();
-                x.items.push_back(what);
-            }
-        }
-    }
-    monkeys.sort_by_key(|m| -m.inspections);
-    println!("part one {:?}", monkeys[..2].iter().map(|m| m.inspections).reduce(|l, r| l * r).unwrap());
-}
-
-fn part_two(data: &str) {
+fn do_work(data: &str, rounds: i32, task: fn(&Monkey, i64, i64) -> i64) {
     let mut monkeys = data.split("\n\n")
         .map(Monkey::new)
         .collect::<Vec<Monkey>>();
     let modulator = monkeys.iter().map(|m| m.test_val).reduce(|a, i| a * i).unwrap();
-    for _ in 0..10_000 {
+    for _ in 0..rounds {
         for midx in 0..monkeys.len() {
             let monkey: &mut Monkey = &mut monkeys[midx];
             let mut throws = Vec::new();
             while let Some(worry_lv) = monkey.items.pop_front() {
                 monkey.inspections += 1;
-                let worry_lv = monkey.operate(worry_lv) % modulator;
+                let worry_lv = task(monkey, worry_lv, modulator);
                 let recipient =
                     if worry_lv % monkey.test_val == 0 { monkey.on_true } else { monkey.on_false };
                 throws.push((worry_lv, recipient));
@@ -114,5 +88,14 @@ fn part_two(data: &str) {
         }
     }
     monkeys.sort_by_key(|m| -m.inspections);
-    println!("part_two {:?}", monkeys[..2].iter().map(|m| m.inspections).reduce(|l, r| l * r).unwrap());
+    println!("{:?}", monkeys[..2].iter().map(|m| m.inspections).reduce(|l, r| l * r).unwrap());
+}
+
+fn part_one(monkey: &Monkey, worry_lv: i64, _: i64) -> i64 {
+    let worry_lv = monkey.operate(worry_lv);
+    (worry_lv as f64 / 3.0) as i64
+}
+
+fn part_two(monkey: &Monkey, worry_lv: i64, modulator: i64) -> i64 {
+    monkey.operate(worry_lv) % modulator
 }
